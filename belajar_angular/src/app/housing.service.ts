1212ea8b7c25522ce9cb44a572b19a0519900b3c
ShellaMarcelia <160854951+ShellaMarcelia@users.noreply.github.com>
2024-11-27 17:41:09 +0700

    commit

--- belajar_angular/src/app/housing.service.ts
+++ belajar_angular/src/app/housing.service.ts
@@ -13,12 +13,39 @@ export class HousingService {
     const data = await fetch(this.url);
     return await data.json() ?? [];
   }
+  
   async getHousingLocationById(id: Number) : Promise<HousingLocation | undefined>{
-    const data = await fetch(`${this.url}/${id}`); //http://localhost:3000/housing
+    const data = await fetch(`${this.url}/${id}`); 
     return await data.json() ??[];
   }
 
   submitApplication(firstName :String, lastName : String, email : String){
-    console.log(firstName, lastName, email)
+    const apiUrl = "http://localhost:3000/register"; 
+
+  const applicationData = {
+    firstname: firstName,
+    lastname: lastName,
+    email: email,
+  };
+
+  return fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json', 
+    },
+    body: JSON.stringify(applicationData), 
+  })
+
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Failed to submit application: ${response.statusText}`);
+      }
+      console.log('Application submitted successfully');
+    })
+
+    .catch((error) => {
+      console.error('Error submitting application:', error);
+      throw error;
+    });
   }
 }
